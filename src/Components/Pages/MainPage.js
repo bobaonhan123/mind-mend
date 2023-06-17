a4e7f93b69e21ecd626a2354775aa140e29d4dce
Bit0 <hosybaonhan2004@gmail.com>
2023-06-17 15:26:11 -0700

    updated animation mainpage

--- src/Components/Pages/MainPage.js
+++ src/Components/Pages/MainPage.js
@@ -1,16 +1,22 @@
-import {memo} from 'react'
+import {memo, useState} from 'react'
 import { Link } from 'react-router-dom';
 
 import '../../Assets/CSS/MainPage.css';
 import NewsData from '../../Data/NewsData';
 import NewsComponent from '../LittleComponent/NewsComponent';
 function MainPage() {
+
+  const [IntroPaneOpacity,setIntroPaneOpacity] = useState({opacity:(window.innerHeight-window.scrollY)/window.innerHeight-(window.scrollY>10?0.2:0)});
+  window.addEventListener("scroll",()=>{
+    setIntroPaneOpacity({opacity:(window.innerHeight-window.scrollY)/window.innerHeight- (window.scrollY>10?0.2:0)});
+  });
+  
   const DataArr = NewsData();
   return (
     <div className="main-page">
           <div className='intro-img'></div>
           <div className='intro-page'>
-            <div className='intro-contentpane'>
+            <div className='intro-contentpane' style={IntroPaneOpacity}>
               <h1>096 306 1414</h1>
               <p><a href='https://duongdaynongngaymai.vn/' style={{'text-decoration':'none','color':'inherit'}} target='blank'>“Đường dây nóng ngày mai”</a> Đường dây nóng hỗ trợ miễn phí người đang trong khủng hoảng tâm lý</p>
             </div>
