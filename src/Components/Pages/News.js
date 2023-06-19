9cbb7bd8240228f2014174f16acfacfabb774654
phuquocchamp <hoangtanphuquoc@gmail.com>
2023-06-19 22:58:33 +0700

    updated content in sharing page

--- src/Components/Pages/News.js
+++ src/Components/Pages/News.js
@@ -16,13 +16,11 @@ SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const News = (props) => {
   const imageList = [
-    { img: './PublicAssets/Img/NewsPage/1.jpg' },
-    { img: './PublicAssets/Img/NewsPage/2.jpg' },
-    { img: './PublicAssets/Img/NewsPage/3.jpg' },
-    { img: './PublicAssets/Img/NewsPage/1.jpg' }
-    
-    
-  ]
+    { img: './PublicAssets/Img/NewsPage/1.jpg', heading: '"Hạnh phúc là trạng thái tâm lý, không phải mục tiêu cuối cùng." - Martin Seligman' },
+    { img: './PublicAssets/Img/NewsPage/2.jpg', heading: '"Sự thay đổi bắt đầu khi bạn quyết định không chấp nhận tình trạng hiện tại."' },
+    { img: './PublicAssets/Img/NewsPage/3.jpg', heading: '"Hãy hiểu tâm lý của bạn, bạn sẽ hiểu được mọi thứ."' },
+    { img: './PublicAssets/Img/NewsPage/1.jpg', heading: '"Sức mạnh của tâm lý xác định sự khác biệt."' }
+  ];
   return (
     <div className="News">
       
@@ -40,16 +38,15 @@ const News = (props) => {
         loop
         style={{ width: '100%', height: '100%' }}
       >
-        {imageList.map((image) => (
-          <SwiperSlide className='swiper__slide'>
+        {imageList.map((image, index) => (
+          <SwiperSlide className='swiper__slide' key={index}>
             <div className="news__header">
-              <h2>Welcome to MindMend - Evolution of Conscious</h2>
+              <h2>{image.heading}</h2>
             </div>
             <img src={image.img} alt="news" />
           </SwiperSlide>
         ))}
-
-      </Swiper> 
+      </Swiper>
 
       {/* Blog Layout */}     
 
@@ -58,18 +55,20 @@ const News = (props) => {
         <div className="left__column">
 
           <div className="news__card">
-            <h2 className="card__heading">The Heading</h2>
-            <h5 className="card__description">Title description</h5>
-            <div className="card__img">Image</div>
-            <p className="card__text">Some Text</p>
+            <h2 className="card__heading">Gửi tôi của nhiều năm về trước</h2>
+            <h5 className="card__description">“Dường như chỉ có tôi, đang một mình dò dẫm trong bóng tối. Cô đơn và tuyệt vọng”</h5>
+            <div className="card__img bg1"></div>
+            <p className="card__text">Hãy tin tớ. Chỉ cần cậu kiên nhẫn, cậu rồi sẽ nhìn thấy lối ra. Lối ra đã ở đó chờ cậu rồi, chỉ cần trái tim cậu sẵn lòng để đón nhận thôi. Bằng cách nào ư? Bằng cách tin tưởng vào chính mình một cách hồn nhiên nhất trong từng khoảnh khắc hiện tại, kể cả khi cậu chưa nắm trong tay bất cứ một bằng chứng nào</p>
+
           </div>
 
 
           <div className="news__card">
-            <h2 className="card__heading">The Heading</h2>
-            <h5 className="card__description">Title description</h5>
-            <div className="card__img">Image</div>
-            <p className="card__text">Some Text</p>
+            <h2 className="card__heading">Về Nhà - Ngay cả khi đang ở nhà, tớ cũng muốn được "về nhà".</h2>
+            <h5 className="card__description">Tớ hy vọng cậu tìm thấy một ngôi nhà ấm cúng bên trong mình, thay vì ước rằng mình có thể nhiều như thế này hơn hay bớt thế kia đi. </h5>
+            <div className="card__img bg2"></div>
+            <p className="card__text">ớ hy vọng cậu vẫn giữ niềm tin ở bản thân, kể cả khi trước mắt cậu chưa phải là những gì cậu muốn nhìn thấy. Tớ hy vọng cậu vẫn sẽ trân trọng và chăm sóc bản thân khi cậu mệt mỏi hay bất lực. Tớ hy vọng sẽ luôn có những điều tốt đẹp bất ngờ xuất hiện trong cuộc sống của cậu.</p>
+
           </div>
 
 
@@ -77,23 +76,22 @@ const News = (props) => {
         <div className="right__column">
           <div className="news__card">
             <h2 className="card__heading">About me</h2>
-            <div className="card__img">
+            <div className="card__img bg3">
             </div>
-            <p className="card__text">Some text about me in culpa qui officia deserunt mollit anim.</p>
-      
+            <p className="card__text">Trưởng thành với cậu là gì? </p>      
           </div>
 
           <div className="news__card">
             <h3 className="card__section">Popular Post</h3>
-            <div className="card__img">Image</div>
-            <div className="card__img">Image</div>
-            <div className="card__img">Image</div>
+            <div className="card__img bg4"></div>
+            <div className="card__img bg5"></div>
+            <div className="card__img bg6"></div>
           </div>
 
 
           <div className="news__card">
-            <h3 className="card__section">Follow Me</h3>
-            <p className="card__text">Some Text</p>
+            <h3 className="card__section">MindMend</h3>
+            <p className="card__text"></p>
           </div>
         </div>
       </div>
