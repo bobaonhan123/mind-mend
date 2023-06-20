d87cff18ae9b9a31f75e09bcbc36ddb156f64904
Bit0 <hosybaonhan2004@gmail.com>
2023-06-20 07:14:31 -0700

    updated news for whatisdepression page

---
+++ src/Data/WhatIsDepressionNewsData.js
@@ -0,0 +1,51 @@
+const data =[
+    {
+        id:1,
+        soucre:'boluminhhiuman',
+        title:'Hãy nói "Cố lên" ít đi',
+        description:`- “Hôm nay cậu có vẻ không ổn?”
+        - Không. Mình không ổn từ rất lâu rồi. Chỉ là hôm nay mình không có đủ năng lượng để che giấu nữa.`,
+        content:`Thực ra, đối với những người mắc bệnh tâm lý, những người đang phải vật lộn với bóng tối bên trong mình, để đến lúc cậu có cơ hội để nói với họ “Cố lên!”, thì đó cũng là lúc họ dường như không thể cố được nữa rồi.
+        Nếu họ vẫn còn cố gắng được thì chắc hẳn cậu sẽ không thể biết sự thật họ đang ở trong hoàn cảnh như vậy.
+        Vậy nên, không phải là họ không ý thức được hay chưa từng nhắc nhở bản thân mình “Cố lên!”, chỉ đơn giản là “sự cố gắng” của họ không giống như những gì cậu thường nghĩ.
+        Có những ngày, hít thở với họ cũng là một loại cố gắng.`,
+        imageURL:'./PublicAssets/Img/News/ID1.jpg',
+        URL:'https://www.facebook.com/boluminhhiuman/posts/pfbid02TjjW1fNGPyhYG612ATBqCMSaBn2Z1uXrMdWEEpEKm8QY64rPJTmRJDPzGybo276bl'
+    },
+    {
+        id:2,
+        soucre:'boluminhhiuman',
+        title:'Gửi tôi của nhiều năm về trước ...',
+        description:'“Dường như chỉ có tôi, đang một mình dò dẫm trong bóng tối. Cô đơn và tuyệt vọng”',
+        content:`Nhưng cậu à, cậu biết điều gì không? Thực ra vì đang ở trong bóng tối của mình nên chúng ta có thể không nhìn thấy được rằng thực tế, mọi người cũng đang mò mẫm và bước đi một cách mơ hồ như vậy. Không có ai là đã biết hết mọi câu trả lời.
+        Có người đi trong bóng tối nhưng tĩnh lặng, có người đi trong ánh sáng nhưng bão bùng, có người đi trong màn sương nhưng lạnh lẽo… Rất nhiều hành trình như thế. Vì vậy, cậu không hề cô đơn và cũng đừng sợ cảm giác cô đơn. Bởi nó không ở đó để làm hại cậu, mà là để nhắc cậu quay trở về quan tâm và yêu thương bản thân thật nhiều.
+        Và cậu biết điều gì thú vị về sự tuyệt vọng không? Đó chính là khi cậu thấy mình tuyệt vọng thì có nghĩa là cậu đã dùng hết những thứ cậu có thể tìm kiếm ở bên ngoài để làm lý do, làm động lực, làm sức mạnh cho mình, và đã đến lúc cậu cần khai phá tiềm năng nằm sâu bên trong để tìm ra những tài nguyên cần thiết cho việc bước tiếp trên hành trình mà cậu đang đi. Chúng không hề ít. Nhưng quan trọng là cậu cần kiên nhẫn đào sâu thêm từng chút để phát hiện.
+        Hãy tin tớ. Chỉ cần cậu kiên nhẫn, cậu rồi sẽ nhìn thấy lối ra. Lối ra đã ở đó chờ cậu rồi, chỉ cần trái tim cậu sẵn lòng để đón nhận thôi. Bằng cách nào ư? Bằng cách tin tưởng vào chính mình một cách hồn nhiên nhất trong từng khoảnh khắc hiện tại, kể cả khi cậu chưa nắm trong tay bất cứ một bằng chứng nào.`,
+        imageURL:'./PublicAssets/Img/News/ID2.jpg',
+        URL:'https://www.facebook.com/boluminhhiuman/posts/pfbid0qvLcJWoJx6jhgESftoakbbQfgQn2yBDGHBBXXfoJJTz4HzWAsYgewL71GCUQLhqel'
+    },
+    {
+        id:3,
+        soucre:'boluminhhiuman',
+        title:'Trầm cảm “trông” như thế nào ?',
+        description:`Khi cậu nói cậu bị trầm cảm
+        Tất cả những gì tớ thấy là sự kiên cường.
+        Không sao cả nếu cậu cảm thấy hỗn loạn và mơ hồ cả ở bên trong lẫn bên ngoài.
+        Điều đó không có nghĩa là cậu thiếu sót, cậu sai hay cậu “hỏng” ở đâu đó.
+        Mà chỉ đơn giản vì cậu cũng là con người.`,
+        content:`Khi cậu nói cậu bị trầm cảm
+        Tất cả những gì tớ thấy là sự kiên cường.
+        Không sao cả nếu cậu cảm thấy hỗn loạn và mơ hồ cả ở bên trong lẫn bên ngoài.
+        Điều đó không có nghĩa là cậu thiếu sót, cậu sai hay cậu “hỏng” ở đâu đó.
+        Mà chỉ đơn giản vì cậu cũng là con người.
+        Hãy nhìn vào mình ở trong gương và nói “tôi biết mình không ổn. Nhưng tôi không sợ điều đó. Thay vì chịu đựng, tôi sẽ chiến đấu mỗi ngày”
+        Cậu không cần phải thắng hay thua. Chỉ đơn giản là cậu đã dũng cảm đấu tranh như thế nào. Đừng từ bỏ hy vọng ở bản thân mình. Cậu vẫn còn hy vọng, kể cả tâm trí cậu đang bảo không phải vậy.`,
+        imageURL:`./PublicAssets/Img/News/ID3.jpg`,
+        URL:'https://www.facebook.com/boluminhhiuman/posts/pfbid0EZbHS42TsJNuULkkBSjqZUyRw8wj2rhe9Pd4V8qMR5Vyh3mBkDHsQ4hgJr1MbTmkl'
+    }
+
+];
+function WhatIsDepressionNewsData() {
+    return data;
+}
+export default WhatIsDepressionNewsData;
